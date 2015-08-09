70cb3f6b3481389af1e5ee4cffb51c0b8c1df066
Wei Zhao <weizhao0411@gmail.com>
2015-08-09 10:55:30 -0700

    First commit

---
+++ src/main/java/com/springcookbook/config/ServletInitializer.java
@@ -0,0 +1,28 @@
+package com.springcookbook.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[0];
+	}
+	
+	/**
+	 * This declares the Spring configuration classes. We declared the AppConfig class
+	 * that is already defined.
+	 */
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[]{AppConfig.class};
+	}
+	
+	/**
+	 * This declares the servlet root URI
+	 */
+	@Override
+	protected String[] getServletMappings() {
+		return new String[]{"/"};
+	}
+}
