8c45911d48e254ece9efe6b90770bce4551a24a8
Wei Zhao <weizhao0411@gmail.com>
2015-08-09 10:58:16 -0700

    Second commit

--- src/main/java/com/springcookbook/config/AppConfig.java
+++ src/main/java/com/springcookbook/config/AppConfig.java
@@ -8,5 +8,5 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @ComponentScan (basePackages = {"com.springcookbook.controller"})
 public class AppConfig {
-
+//This is a dummy comment
 }
